ba941c834f0d7a1814f749882157ff8102c547cd
Matt Czencz <mattczencz24@gmail.com>
2023-06-02 17:02:56 -0600

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['links.papareact.com']
+    domains: [ 'links.papareact.com' ]
   },
   experimental: {
     appDir: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 };
 
